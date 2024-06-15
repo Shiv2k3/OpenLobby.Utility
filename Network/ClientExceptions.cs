7624e5f2d084dacacae3cedfdd149a5c8ddbabeb
Shiv2k3 <78758913+Shiv2k3@users.noreply.github.com>
2024-06-15 12:16:08 -0500

    added

---
+++ Network/ClientExceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OpenLobby.Utility.Network
+{
+    /// <summary>
+    /// The address was already in use
+    /// </summary>
+    public class AddressInUse : Exception
+    {
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        public AddressInUse() : base("The address was already in use, the socket has been disposed") { }
+    }
+}
